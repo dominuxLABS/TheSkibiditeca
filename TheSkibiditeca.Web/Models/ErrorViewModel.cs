0759ca3c3d83b1c8a30aa8ee284d40eadfc08dcc
dom1nux <go.francko.dr@gmail.com>
2025-08-07 11:57:36 -0500

    Add project files.

---
+++ TheSkibiditeca.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheSkibiditeca.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
