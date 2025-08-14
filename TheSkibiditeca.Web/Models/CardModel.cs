fee57ee1de6d44db7b3f312813b4034b6972998d
Galaxy <luisirigoin123@gmail.com>
2025-08-13 23:52:07 -0500

    Página principal actualizada + Partial de cards
    Nueva página de inicio, no se si será la versión final, viendo pez, también diseño de los cards, es lo que mas me tomó tiempo XD

---
+++ TheSkibiditeca.Web/Models/CardModel.cs
@@ -0,0 +1,7 @@
+namespace TheSkibiditeca.Web.Models {
+    public class CardModel {
+        public required string Image;
+        public required string Title;
+        public required string Description;
+    }
+}
