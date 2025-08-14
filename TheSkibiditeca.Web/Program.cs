d339ae4983835195dd31cd3d4a18ee1f8ad49f7b
dom1nux <go.francko.dr@gmail.com>
2025-08-14 08:02:45 -0500

    refactor: enhance Program.cs with Entity Framework setup and database seeding

--- TheSkibiditeca.Web/Program.cs
+++ TheSkibiditeca.Web/Program.cs
@@ -1,14 +1,32 @@
+// Copyright (c) dominuxLABS. All rights reserved.
+
+using Microsoft.EntityFrameworkCore;
+using TheSkibiditeca.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Add Entity Framework
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Seed the database
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+    context.Database.Migrate();
+    DbSeeder.SeedData(context);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
