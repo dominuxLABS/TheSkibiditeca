56df0bdd5e0fac815d8387981ffc4d033b26f82c
dom1nux <go.francko.dr@gmail.com>
2025-08-15 16:13:18 -0500

    refactor: add migration for PasswordHash and PasswordSalt fields to User entity

---
+++ TheSkibiditeca.Web/Migrations/20250815210801_AddPasswordFieldsToUser.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheSkibiditeca.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordFieldsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+        }
+    }
+}
