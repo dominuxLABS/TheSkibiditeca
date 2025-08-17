3c428ac5ff4d02d7095e447eed9779a39607e8ba
dom1nux <go.francko.dr@gmail.com>
2025-08-17 17:16:39 -0500

    refactor: add migration to drop Loan/Reservation association with Book and update related entities

---
+++ TheSkibiditeca.Web/Migrations/Npgsql/20250817221609_DropLoanReservationBookAssociation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheSkibiditeca.Web.Migrations.Npgsql
+{
+    /// <inheritdoc />
+    public partial class DropLoanReservationBookAssociation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
