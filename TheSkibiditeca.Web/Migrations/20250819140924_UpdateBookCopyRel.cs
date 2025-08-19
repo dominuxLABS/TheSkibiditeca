23813111c4f25b11d2788e1cd74ba0ce62133f78
dom1nux <go.francko.dr@gmail.com>
2025-08-19 09:55:09 -0500

    feat: update migration to remove BookId foreign key from Loans and Reservations tables

---
+++ TheSkibiditeca.Web/Migrations/20250819140924_UpdateBookCopyRel.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheSkibiditeca.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookCopyRel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Books_BookId",
+                table: "Loans");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Books_BookId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_BookId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Loans_BookId",
+                table: "Loans");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Loans");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "Reservations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "Loans",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_BookId",
+                table: "Reservations",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Loans_BookId",
+                table: "Loans",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Books_BookId",
+                table: "Loans",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Books_BookId",
+                table: "Reservations",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId");
+        }
+    }
+}
