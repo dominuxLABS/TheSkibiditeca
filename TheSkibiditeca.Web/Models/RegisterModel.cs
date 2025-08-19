cf6857f7dca4c3f2af81715a24ed20ff13faeddc
dom1nux <go.francko.dr@gmail.com>
2025-08-19 09:54:52 -0500

    chore: add copyright notice and format RegisterModel class

--- TheSkibiditeca.Web/Models/RegisterModel.cs
+++ TheSkibiditeca.Web/Models/RegisterModel.cs
@@ -1,4 +1,11 @@
-namespace TheSkibiditeca.Web.Models {
-    public class RegisterModel {
+// Copyright (c) dominuxLABS. All rights reserved.
+
+namespace TheSkibiditeca.Web.Models
+{
+    /// <summary>
+    /// Represents the model for user registration data.
+    /// </summary>
+    public class RegisterModel
+    {
     }
 }
