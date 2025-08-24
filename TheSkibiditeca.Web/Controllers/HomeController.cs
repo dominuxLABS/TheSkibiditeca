170037d24732e8528510f2e3a45dface3ba84bd2
dom1nux <go.francko.dr@gmail.com>
2025-08-24 04:00:28 -0500

    refactor: enhance code structure and documentation in HomeController

--- TheSkibiditeca.Web/Controllers/HomeController.cs
+++ TheSkibiditeca.Web/Controllers/HomeController.cs
@@ -1,9 +1,39 @@
+// Copyright (c) dominuxLABS. All rights reserved.
+
 using Microsoft.AspNetCore.Mvc;
 
-namespace TheSkibiditeca.Web.Controllers {
-    public class HomeController : Controller {
-        public IActionResult Index() { return View(); }
-        public IActionResult Privacy() { return View(); }
-        public IActionResult Credits() { return View(); }
+namespace TheSkibiditeca.Web.Controllers
+{
+    /// <summary>
+    /// The main site controller for top-level pages.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Shows the home/index page.
+        /// </summary>
+        /// <returns>The Index view.</returns>
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Shows the privacy page.
+        /// </summary>
+        /// <returns>The Privacy view.</returns>
+        public IActionResult Privacy()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// Shows the credits page.
+        /// </summary>
+        /// <returns>The Credits view.</returns>
+        public IActionResult Credits()
+        {
+            return this.View();
+        }
     }
 }
