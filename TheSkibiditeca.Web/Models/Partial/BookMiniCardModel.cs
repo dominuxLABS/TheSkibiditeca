4c178b203c73f79e797211f529e999b203b3ca17
dom1nux <go.francko.dr@gmail.com>
2025-08-24 04:01:49 -0500

    refactor: improve code structure and documentation in BookMiniCardModel

--- TheSkibiditeca.Web/Models/Partial/BookMiniCardModel.cs
+++ TheSkibiditeca.Web/Models/Partial/BookMiniCardModel.cs
@@ -1,7 +1,25 @@
-namespace TheSkibiditeca.Web.Models.Partial {
-    public class BookMiniCardModel {
+// Copyright (c) dominuxLABS. All rights reserved.
+
+namespace TheSkibiditeca.Web.Models.Partial
+{
+    /// <summary>
+    /// Lightweight model used to render a small card for a book (image, title and id) in lists or partial views.
+    /// </summary>
+    public class BookMiniCardModel
+    {
+        /// <summary>
+        /// Gets or sets the URL of the book cover image.
+        /// </summary>
         public required string ImageURL { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the book.
+        /// </summary>
         public required string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the book identifier as a string (used for links/routes).
+        /// </summary>
         public required string BookID { get; set; }
     }
 }
