892ee2e83217afd787f7447125f17fe0821b42f5
dom1nux <go.francko.dr@gmail.com>
2025-08-24 04:00:21 -0500

    refactor: improve code structure and documentation in ListController

--- TheSkibiditeca.Web/Controllers/ListController.cs
+++ TheSkibiditeca.Web/Controllers/ListController.cs
@@ -1,13 +1,30 @@
+// Copyright (c) dominuxLABS. All rights reserved.
+
 using Microsoft.AspNetCore.Mvc;
 
-namespace TheSkibiditeca.Web.Controllers {
-    public class ListController : Controller {
-        public IActionResult Index() {
-            return RedirectToAction("Home", "Index");
+namespace TheSkibiditeca.Web.Controllers
+{
+    /// <summary>
+    /// Controller for listing pages (books, categories, etc.).
+    /// </summary>
+    public class ListController : Controller
+    {
+        /// <summary>
+        /// Redirects to the home index page.
+        /// </summary>
+        /// <returns>A redirect result to Home/Index.</returns>
+        public IActionResult Index()
+        {
+            return this.RedirectToAction("Index", "Home");
         }
 
-        public IActionResult Book() {
-            return View();
+        /// <summary>
+        /// Shows the book list view.
+        /// </summary>
+        /// <returns>The Book view.</returns>
+        public IActionResult Book()
+        {
+            return this.View();
         }
     }
 }
