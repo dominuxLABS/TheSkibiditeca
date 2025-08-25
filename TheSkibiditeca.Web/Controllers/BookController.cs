9e9e2d1bdee4fe3711ccb9669b482216fd119ef3
Galaxy <luisirigoin123@gmail.com>
2025-08-25 02:49:31 -0500

    refactor: Cambié todo el sistema pq estaba alrevez xd

---
+++ TheSkibiditeca.Web/Controllers/BookController.cs
@@ -0,0 +1,108 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TheSkibiditeca.Web.Data;
+using TheSkibiditeca.Web.Models.Create;
+using TheSkibiditeca.Web.Models.Entities;
+using TheSkibiditeca.Web.Models.ModelPartial;
+
+namespace TheSkibiditeca.Web.Controllers {
+    
+    public class BookController : Controller {
+        private readonly LibraryDbContext db;
+        private readonly UserManager<User> _userM;
+        public BookController(LibraryDbContext dbo, UserManager<User> user) {
+            db = dbo;
+            _userM = user;
+        }
+        public IActionResult Index() {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create() {
+            ViewBag.Categories = db.Categories.ToArray();
+            ViewBag.Authors = db.Authors.ToArray();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BookCreateModel bmodel) {
+            Book nbook = bmodel.book;
+            db.Books.Add(nbook);
+            db.SaveChanges();
+            Book lastAdded = db.Books.OrderBy(e => e.BookId).LastOrDefault();
+            for(int i = 0; i < bmodel.Copies; i++) {
+                db.Copies.Add(new Copy() {
+                    BookId = lastAdded.BookId,
+                    ISBN = DbSeeder.GenerateIsbn(),
+                    PublisherName = "GenericPublisher",
+                    PhysicalLocation = "GenericPosition",
+                    IsActive = true
+                });
+            }
+
+            foreach(string authorId in bmodel.authors) {
+                db.BookAuthors.Add(new BookAuthor() {
+                    BookId = lastAdded.BookId,
+                    AuthorId = int.Parse(authorId),
+                    Role = "Writer"
+                });
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Book", "List");
+        }
+
+        public async Task<IActionResult> List(string? searchStr, int page = 1, int pageSize = 30) {
+            var allBooks = new List<BookMiniCardModel>();
+            foreach(Book b in db.Books) {
+                allBooks.Add(new BookMiniCardModel() {
+                    BookID = b.BookId.ToString(),
+                    Title = b.Title,
+                    ImageURL = b.CoverImageUrl
+                });
+            }
+
+            if(!String.IsNullOrEmpty(searchStr)) {
+                allBooks = allBooks.Where(b => b.Title.Contains(searchStr, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+            var paginatedBooks = allBooks.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            ViewBag.AllBooks = paginatedBooks;
+            var user = await _userM.GetUserAsync(HttpContext.User);
+            if(user != null) { ViewBag.RoleID = user.UserTypeId; }
+            ViewData["CurrentPage"] = page;
+            ViewData["CurrentFilter"] = searchStr;
+            ViewData["PageSize"] = pageSize;
+            ViewData["TotalPages"] = (int)Math.Ceiling(allBooks.Count / (double)pageSize);
+            return this.View();
+        }
+
+        public async Task<IActionResult> Details(string? bookId) {
+            if(!int.TryParse(bookId, out var id)) {
+                id = 1;
+            }
+
+            var user = await _userM.GetUserAsync(HttpContext.User);
+            if(user != null) { ViewBag.RoleID = user.UserTypeId; }
+
+            var c = this.db.Books.Find(id);
+            if(c == null) {
+                return this.NotFound();
+            }
+
+            // log a serialized copy for debugging (kept intentionally simple)
+            var authorsIds = this.db.BookAuthors.Where(e => e.BookId == c.BookId).Select(e => e.AuthorId);
+            var authorNames = this.db.Authors.Where(e => authorsIds.Contains(e.AuthorId)).Select(e => e.FullName);
+
+            this.ViewBag.Title = c.Title;
+            this.ViewBag.Description = c.Description;
+            this.ViewBag.Year = c.PublicationYear;
+            this.ViewBag.Authors = String.Join(",", authorNames);
+            this.ViewBag.Count = this.db.Copies.Where(e => e.BookId == c.BookId).Count();
+            this.ViewBag.URL = c.CoverImageUrl;
+            return this.View();
+        }
+    }
+}
---
+++ TheSkibiditeca.Web/Controllers/LoanController.cs
@@ -0,0 +1,150 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TheSkibiditeca.Web.Data;
+using TheSkibiditeca.Web.Models.Entities;
+
+public class LoanController : Controller
+{
+    private readonly LibraryDbContext _context;
+
+    public LoanController(LibraryDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: LOANS
+    public async Task<IActionResult> Index()    
+    {
+        return View(await _context.Loans.ToListAsync());
+    }
+
+    // GET: LOANS/Details/5
+    public async Task<IActionResult> Details(int? loanid)
+    {
+        if (loanid == null)
+        {
+            return NotFound();
+        }
+
+        var loan = await _context.Loans
+            .FirstOrDefaultAsync(m => m.LoanId == loanid);
+        if (loan == null)
+        {
+            return NotFound();
+        }
+
+        return View(loan);
+    }
+
+    // GET: LOANS/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: LOANS/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("ID,Title,ReleaseDate,Genre,Price")] Loan loan)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(loan);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(loan);
+    }
+
+    // GET: LOANS/Edit/5
+    public async Task<IActionResult> Edit(int? loanid)
+    {
+        if (loanid == null)
+        {
+            return NotFound();
+        }
+
+        var loan = await _context.Loans.FindAsync(loanid);
+        if (loan == null)
+        {
+            return NotFound();
+        }
+        return View(loan);
+    }
+
+    // POST: LOANS/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? loanid, [Bind("ID,Title,ReleaseDate,Genre,Price")] Loan loan)
+    {
+        if (loanid != loan.LoanId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(loan);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LoanExists(loan.LoanId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(loan);
+    }
+
+    // GET: LOANS/Delete/5
+    public async Task<IActionResult> Delete(int? loanid)
+    {
+        if (loanid == null)
+        {
+            return NotFound();
+        }
+
+        var loan = await _context.Loans
+            .FirstOrDefaultAsync(m => m.LoanId == loanid);
+        if (loan == null)
+        {
+            return NotFound();
+        }
+
+        return View(loan);
+    }
+
+    // POST: LOANS/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? loanid)
+    {
+        var loan = await _context.Loans.FindAsync(loanid);
+        if (loan != null)
+        {
+            _context.Loans.Remove(loan);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool LoanExists(int? loanid)
+    {
+        return _context.Loans.Any(e => e.LoanId == loanid);
+    }
+}
