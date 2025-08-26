9339136a2c55a3c45418e65fadad38786322b336
dom1nux <go.francko.dr@gmail.com>
2025-08-26 04:43:42 -0500

    refactor: update AuthorCreateModel structure and improve property naming conventions

--- TheSkibiditeca.Web/Models/AuthorModels/AuthorCreateModel.cs
+++ TheSkibiditeca.Web/Models/AuthorModels/AuthorCreateModel.cs
@@ -1,7 +1,16 @@
+// Copyright (c) dominuxLABS. All rights reserved.
+
 using TheSkibiditeca.Web.Models.Entities;
 
-namespace TheSkibiditeca.Web.Models.AuthorModels {
-    public class AuthorCreateModel {
-        public Author author {  get; set; }
-    }
+namespace TheSkibiditeca.Web.Models.AuthorModels;
+
+/// <summary>
+/// Model used when creating an author.
+/// </summary>
+public class AuthorCreateModel
+{
+    /// <summary>
+    /// Gets or sets the author to create.
+    /// </summary>
+    public required Author Author { get; set; }
 }
