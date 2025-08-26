d5b8587415d1327f2f76f1c9dccd3c0bfb5de8a8
dom1nux <go.francko.dr@gmail.com>
2025-08-26 04:59:01 -0500

    refactor: enhance formatting and structure in EditLoanModel for better readability

--- TheSkibiditeca.Web/Models/LoanModels/EditLoanModel.cs
+++ TheSkibiditeca.Web/Models/LoanModels/EditLoanModel.cs
@@ -1,9 +1,26 @@
+// Copyright (c) dominuxLABS. All rights reserved.
+
 using TheSkibiditeca.Web.Models.Entities;
 
-namespace TheSkibiditeca.Web.Models.LoanModels {
-    public class EditLoanModel {
-        public Loan loan { get; set; }
-        public List<Copy> copies { get; set; }
-        public List<Book> books { get; set; }
-    }
+namespace TheSkibiditeca.Web.Models.LoanModels;
+
+/// <summary>
+/// Represents the model used to edit a loan, including the loan itself and related copies and books.
+/// </summary>
+public class EditLoanModel
+{
+    /// <summary>
+    /// Gets or sets the loan being edited.
+    /// </summary>
+    public Loan Loan { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the list of copies associated with the loan.
+    /// </summary>
+    public List<Copy> Copies { get; set; } = [];
+
+    /// <summary>
+    /// Gets or sets the list of books associated with the loan.
+    /// </summary>
+    public List<Book> Books { get; set; } = [];
 }
