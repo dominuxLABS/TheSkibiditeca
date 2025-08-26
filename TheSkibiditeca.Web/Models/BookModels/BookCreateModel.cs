41f28df26372a08bdfbc496916567cf7da3843da
dom1nux <go.francko.dr@gmail.com>
2025-08-26 04:46:15 -0500

    refactor: update BookCreateModel structure and improve property naming conventions

--- TheSkibiditeca.Web/Models/BookModels/BookCreateModel.cs
+++ TheSkibiditeca.Web/Models/BookModels/BookCreateModel.cs
@@ -1,9 +1,26 @@
+// Copyright (c) dominuxLABS. All rights reserved.
+
 using TheSkibiditeca.Web.Models.Entities;
 
-namespace TheSkibiditeca.Web.Models.BookModels {
-    public class BookCreateModel {
-        public Book book { get; set; }
-        public List<string> authors { get; set; }
-        public int Copies { get; set; }
-    }
+namespace TheSkibiditeca.Web.Models.BookModels;
+
+/// <summary>
+/// Model used to create a new book along with its authors and copy count.
+/// </summary>
+public class BookCreateModel
+{
+    /// <summary>
+    /// Gets or sets the book entity to create.
+    /// </summary>
+    public required Book Book { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of author names associated with the book.
+    /// </summary>
+    public required List<string> Authors { get; set; }
+
+    /// <summary>
+    /// Gets or sets the number of copies to create.
+    /// </summary>
+    public int Copies { get; set; }
 }
