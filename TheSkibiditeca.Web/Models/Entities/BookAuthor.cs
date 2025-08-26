7e0d90d4ae222290aef96fc92b7e3f0a57368ba1
dom1nux <go.francko.dr@gmail.com>
2025-08-26 04:58:02 -0500

    refactor: improve formatting and structure in BookAuthor model for better readability

--- TheSkibiditeca.Web/Models/Entities/BookAuthor.cs
+++ TheSkibiditeca.Web/Models/Entities/BookAuthor.cs
@@ -3,38 +3,37 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TheSkibiditeca.Web.Models.Entities
+namespace TheSkibiditeca.Web.Models.Entities;
+
+/// <summary>
+/// Represents the many-to-many relationship between books and authors.
+/// </summary>
+[Table("BookAuthors")]
+public class BookAuthor
 {
     /// <summary>
-    /// Represents the many-to-many relationship between books and authors.
+    /// Gets or sets the book identifier.
     /// </summary>
-    [Table("BookAuthors")]
-    public class BookAuthor
-    {
-        /// <summary>
-        /// Gets or sets the book identifier.
-        /// </summary>
-        public int BookId { get; set; }
+    public int BookId { get; set; }
 
-        /// <summary>
-        /// Gets or sets the author identifier.
-        /// </summary>
-        public int AuthorId { get; set; }
+    /// <summary>
+    /// Gets or sets the author identifier.
+    /// </summary>
+    public int AuthorId { get; set; }
 
-        /// <summary>
-        /// Gets or sets the author's role for this book.
-        /// </summary>
-        [StringLength(50)]
-        public string Role { get; set; } = "Author";
+    /// <summary>
+    /// Gets or sets the author's role for this book.
+    /// </summary>
+    [StringLength(50)]
+    public string Role { get; set; } = "Author";
 
-        /// <summary>
-        /// Gets or sets the book navigation property.
-        /// </summary>
-        public virtual Book Book { get; set; } = null!;
+    /// <summary>
+    /// Gets or sets the book navigation property.
+    /// </summary>
+    public virtual Book Book { get; set; } = null!;
 
-        /// <summary>
-        /// Gets or sets the author navigation property.
-        /// </summary>
-        public virtual Author Author { get; set; } = null!;
-    }
+    /// <summary>
+    /// Gets or sets the author navigation property.
+    /// </summary>
+    public virtual Author Author { get; set; } = null!;
 }
